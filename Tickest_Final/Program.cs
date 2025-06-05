8a0e6f03224b1812650f6e542e1efebc11fdef43
diego <diego@LAPTOP-0PV5A95C>
2025-06-05 14:04:32 -0600

    Creacion de multipes views

--- Tickest_Final/Program.cs
+++ Tickest_Final/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Tickest_Final.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configuración de la cadena de conexión a la base de datos
+builder.Services.AddDbContext<ticketsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(""))
+);
 
 var app = builder.Build();
 
@@ -9,19 +17,22 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // El valor predeterminado de HSTS es 30 días. Podrías cambiarlo para escenarios de producción.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+// Necesario para manejar las rutas
 app.UseRouting();
 
+// Para manejar la autorización
 app.UseAuthorization();
 
+// Configura los endpoints de la aplicación
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Login}/{id?}");
 
 app.Run();
